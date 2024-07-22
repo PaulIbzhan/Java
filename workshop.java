43a5c4f5042cfe537911ff346a4fea9cbc1c19f2
Paul Ibzhan <143822001+PaulIbzhan@users.noreply.github.com>
2024-07-22 10:05:15 +0530

    Update workshop.java

--- workshop.java
+++ workshop.java
@@ -1,20 +1,10 @@
-package javaworkshop;
-
-import java.util.ArrayList;
-import java.util.ListIterator;
-public class workshop {
+public class firstprogram {
 	public static void main(String[] args) {
-		ArrayList<Integer> numbers= new ArrayList<>();
-		numbers.add(1);
-		numbers.add(2);
-		numbers.add(3);
-		System.out.print("ArrayList: "+numbers);
-		
-		ListIterator<Integer> iterate = numbers.listIterator();
-		int number=iterate.next();
-	    System.out.println("Next Element: " + number);
-	     
-	    int index=iterate.nextIndex();
-	    System.out.print(index);
+		final int a[]={1,2,3,4,5};
+		a[i]=20;
+		int sum = 0;
+		for(int i:a)
+			sum+=i;
+		System.out.print(sum);
 	}
 }
